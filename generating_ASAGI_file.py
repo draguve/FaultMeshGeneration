bc74e4339e95fb86497b3b035456fbd4ec162e8c
Draguve <ritwikpatil1998@gmail.com>
2024-07-15 16:38:30 -0700

    output looks corrected but incorrectly rotated

--- generating_ASAGI_file.py
+++ generating_ASAGI_file.py
@@ -65,41 +65,42 @@ def writeNetcdf4SeisSol(sname, x, y, z, aName, aData):
     rootgrp.close()
 
 
-x = np.linspace(-50000, 50000, int((50000 + 50000) / 100) + 1)  # for testing if correct
-y = np.linspace(0, 10000, int(10000 / 100))
-z = np.linspace(-50000, 50000, int((50000 + 50000) / 100))
+if __name__ == '__main__':
+    x = np.linspace(-50000, 50000, int((50000 + 50000) / 100) + 1)  # for testing if correct
+    y = np.linspace(0, 10000, int(10000 / 100))
+    z = np.linspace(-50000, 50000, int((50000 + 50000) / 100))
 
-# Generate a Gaussian shaped slip distribution for illustration purposes
-xg, yg, zg = np.meshgrid(x, y, z, indexing='ij')
-Rx = np.zeros(xg.shape)
-Rz = np.zeros(xg.shape)
+    # Generate a Gaussian shaped slip distribution for illustration purposes
+    xg, yg, zg = np.meshgrid(x, y, z, indexing='ij')
+    Rx = np.zeros(xg.shape)
+    Rz = np.zeros(xg.shape)
 
-loc_temp = xg < -9800
-Rx[loc_temp] = (-xg[loc_temp] - 9800) / 10000
-loc_temp = xg > 1100
-Rx[loc_temp] = (xg[loc_temp] - 1100) / 10000
+    loc_temp = xg < -9800
+    Rx[loc_temp] = (-xg[loc_temp] - 9800) / 10000
+    loc_temp = xg > 1100
+    Rx[loc_temp] = (xg[loc_temp] - 1100) / 10000
 
-loc_temp = zg < -8000
-Rz[loc_temp] = (-zg[loc_temp] - 8000) / 1000
-loc_temp = zg > -2300
-Rz[loc_temp] = (zg[loc_temp] + 2300) / 10000
+    loc_temp = zg < -8000
+    Rz[loc_temp] = (-zg[loc_temp] - 8000) / 1000
+    loc_temp = zg > -2300
+    Rz[loc_temp] = (zg[loc_temp] + 2300) / 10000
 
-Rt = np.minimum(1, np.sqrt(np.power(Rx, 2) + np.power(Rz, 2)))
-s_xy0 = 30000000.0 * (1.0 - Rt)
+    Rt = np.minimum(1, np.sqrt(np.power(Rx, 2) + np.power(Rz, 2)))
+    s_xy0 = 30000000.0 * (1.0 - Rt)
 
-radius = np.sqrt(np.power(xg + 6000, 2) + np.power(zg + 6000, 2))
-pi = 4 * math.atan(1.0)
+    radius = np.sqrt(np.power(xg + 6000, 2) + np.power(zg + 6000, 2))
+    pi = 4 * math.atan(1.0)
 
-s_xy1 = np.zeros(xg.shape)
-loc_temp = radius <= 550
-s_xy1[loc_temp] = 3150000
-loc_temp = 1575000. * (1.0 + np.cos(pi * (radius - 550.0) / 250.0))
+    s_xy1 = np.zeros(xg.shape)
+    loc_temp = radius <= 550
+    s_xy1[loc_temp] = 3150000
+    loc_temp = 1575000. * (1.0 + np.cos(pi * (radius - 550.0) / 250.0))
 
-sheer_stress = s_xy0 + s_xy1
+    sheer_stress = s_xy0 + s_xy1
 
-prefix = "test"
-ldataName = ["sheer_stress"]
-lgridded_myData = [sheer_stress]
+    prefix = "test"
+    ldataName = ["sheer_stress"]
+    lgridded_myData = [sheer_stress]
 
-writeNetcdf4Paraview(prefix, x, y, z, ldataName, lgridded_myData)
-writeNetcdf4SeisSol(prefix, x, y, z, ldataName, lgridded_myData)
+    writeNetcdf4Paraview(prefix, x, y, z, ldataName, lgridded_myData)
+    writeNetcdf4SeisSol(prefix, x, y, z, ldataName, lgridded_myData)
