0fda43d3e975119570b3bfe8de865688f72ddfed
Draguve <ritwikpatil1998@gmail.com>
2024-10-07 22:07:16 -0700

    fixed connected lines in plot

--- generate_svg.py
+++ generate_svg.py
@@ -24,9 +24,19 @@ def main(input_filename="outputs/BayModel1/merged_catmul_500m_a0.5.csv",
          tr_lat=39.45517835297083, tr_lon=-120.27386,
          bl_lat=35.72582999999996, bl_lon=-123.68866751003426):
     data = read_csv(input_filename)
-    utm_x = []
-    utm_y = []
+
+    proj = Proj(proj='utm', zone=get_utm_zone(tr_lon), ellps='WGS84', preserve_units=False)
+    utm_tr_x, utm_tr_y = proj(tr_lon, tr_lat)
+    proj = Proj(proj='utm', zone=get_utm_zone(bl_lon), ellps='WGS84', preserve_units=False)
+    utm_bl_x, utm_bl_y = proj(bl_lon, bl_lat)
+
+
+    fig = plt.figure(frameon=False,figsize=(10,10*((utm_tr_x - utm_bl_x) / (utm_tr_y - utm_bl_y))))
+    ax = fig.add_axes([0, 0, 1, 1])
+
     for line in data:
+        utm_x = []
+        utm_y = []
         line_lat_longs = get_points(line[-1])
         utm_zones = [get_utm_zone(lon_lat[0]) for lon_lat in line_lat_longs]
         for i, lon_lat in enumerate(line_lat_longs):
@@ -34,15 +44,8 @@ def main(input_filename="outputs/BayModel1/merged_catmul_500m_a0.5.csv",
             x, y = proj(lon_lat[0], lon_lat[1])
             utm_x.append(x)
             utm_y.append(y)
+            plt.plot(utm_x, utm_y, marker='o', linestyle='-')
 
-    proj = Proj(proj='utm', zone=get_utm_zone(tr_lon), ellps='WGS84', preserve_units=False)
-    utm_tr_x, utm_tr_y = proj(tr_lon, tr_lat)
-    proj = Proj(proj='utm', zone=get_utm_zone(bl_lon), ellps='WGS84', preserve_units=False)
-    utm_bl_x, utm_bl_y = proj(bl_lon, bl_lat)
-
-    fig = plt.figure(frameon=False,figsize=(10,10*((utm_tr_x - utm_bl_x) / (utm_tr_y - utm_bl_y))))
-    ax = fig.add_axes([0, 0, 1, 1])
-    plt.plot(utm_x, utm_y, marker='o', linestyle='-')
     plt.title('Projected UTM Coordinates')
     plt.xlabel('UTM Easting (m)')
     plt.ylabel('UTM Northing (m)')
@@ -56,7 +59,7 @@ def main(input_filename="outputs/BayModel1/merged_catmul_500m_a0.5.csv",
     plt.axis('off')  # Turn off the axis
 
     # Save the plot as an SVG file
-    plt.savefig('utm_coordinates_plot.svg', format='svg',dpi=300)
+    plt.savefig('utm_coordinates_plot.svg', format='svg')
 
     # Show the plot (optional)
     plt.show()
