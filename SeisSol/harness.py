374bb4c2951ecfed312566bb43192f4eb1061185
draguve <draguve@gmail.com>
2025-09-23 10:56:33 -0700

    Units

--- SeisSol/harness.py
+++ SeisSol/harness.py
@@ -1,39 +1,71 @@
-import os
-import pandas as pd
-import numpy as np
-import matplotlib.pylab as plt
 import argparse
-from pathlib import Path
+import os
 import sys
+from pathlib import Path
+
+import matplotlib.pylab as plt
+import numpy as np
+import pandas as pd
+
+
+def get_units(variable_name):
+    units = {
+        "gravitational_energy": "J",
+        "acoustic_energy": "J",
+        "acoustic_kinetic_energy": "J",
+        "elastic_energy": "J",
+        "elastic_kinetic_energy": "J",
+        "momentumX": "kg·m/s",
+        "momentumY": "kg·m/s",
+        "momentumZ": "kg·m/s",
+        "total_frictional_work": "J",
+        "static_frictional_work": "J",
+        "seismic_moment": "N·m",
+        "potency": "m³",
+        "plastic_moment": "J",
+        "seismic_moment_rate": "N·m/s",
+        "M_0": "N·m",
+        "M_w": "",
+    }
+    return units.get(variable_name, "")
+
 
 def parse_parameters(file_path):
     output = {}
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         for line in file:
             # Remove comments
-            line = line.split('!')[0].strip()
-            if '=' in line:
-                key, value = line.split('=', 1)
+            line = line.split("!")[0].strip()
+            if "=" in line:
+                key, value = line.split("=", 1)
                 key = key.strip()
                 value = value.strip().strip("'\"")  # Remove quotes if any
                 output[key] = value
     return output
 
-def plot_and_save(df,what_to_plot,plots_path):
-    df.plot(y=what_to_plot,use_index=True)
-    plt.savefig(f"{plots_path}{what_to_plot}.png", dpi=300, bbox_inches='tight')
+
+def plot_and_save(df, what_to_plot, plots_path):
+    df.plot(
+        y=what_to_plot,
+        use_index=True,
+        xlabel="Time (s)",
+        ylabel=f"{what_to_plot} in {get_units(what_to_plot)}",
+    )
+    plt.savefig(f"{plots_path}{what_to_plot}.png", dpi=300, bbox_inches="tight")
+
 
 def before(params):
-    output_prefix = os.path.abspath(params.get('OutputFile'))
+    output_prefix = os.path.abspath(params.get("OutputFile"))
     output_dir = os.path.dirname(output_prefix)
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     print(output_dir)
+
 
 def after(params):
     if int(params.get("EnergyOutput")) != 1:
         print("Energy Output is disabled")
         exit(0)
-    output_prefix = params.get('OutputFile')
+    output_prefix = params.get("OutputFile")
     plots_path = f"{output_prefix}-plots/"
     if not os.path.exists(plots_path):
         os.makedirs(plots_path)
@@ -44,47 +76,55 @@ def after(params):
     print("Generating plots...")
     df = pd.read_csv(energy_file)
     df = df.pivot_table(index="time", columns="variable", values="measurement")
-    df["seismic_moment_rate"] = np.gradient(df["seismic_moment"], df.index[1]) #df index gets the time difference between 2 points
-    plot_and_save(df,"seismic_moment_rate",plots_path)
-    df["M_0"] = df["seismic_moment"] - df["total_frictional_work"] - df["elastic_energy"] - df["elastic_kinetic_energy"]
+    df["seismic_moment_rate"] = np.gradient(
+        df["seismic_moment"], df.index[1]
+    )  # df index gets the time difference between 2 points
+    plot_and_save(df, "seismic_moment_rate", plots_path)
+    df["M_0"] = (
+        df["seismic_moment"]
+        - df["total_frictional_work"]
+        - df["elastic_energy"]
+        - df["elastic_kinetic_energy"]
+    )
     df["M_0"] = df["M_0"].where(df["M_0"] > 0)  # mask non-positive values
     df["M_w"] = (np.log10(df["M_0"]) - 9.1) / 1.5
 
-    plot_and_save(df,"M_0",plots_path)
-    plot_and_save(df,"M_w",plots_path)
+    plot_and_save(df, "M_0", plots_path)
+    plot_and_save(df, "M_w", plots_path)
 
-    plot_and_save(df,"acoustic_energy",plots_path)
-    plot_and_save(df,"acoustic_kinetic_energy",plots_path)
-    plot_and_save(df,"elastic_energy",plots_path)
-    plot_and_save(df,"elastic_kinetic_energy",plots_path)
-    plot_and_save(df,"gravitational_energy",plots_path)
-    plot_and_save(df,"momentumX",plots_path)
-    plot_and_save(df,"momentumY",plots_path)
-    plot_and_save(df,"momentumZ",plots_path)
-    plot_and_save(df,"plastic_moment",plots_path)
-    plot_and_save(df,"potency",plots_path)
-    plot_and_save(df,"seismic_moment",plots_path)
-    plot_and_save(df,"static_frictional_work",plots_path)
-    plot_and_save(df,"total_frictional_work",plots_path)
+    plot_and_save(df, "acoustic_energy", plots_path)
+    plot_and_save(df, "acoustic_kinetic_energy", plots_path)
+    plot_and_save(df, "elastic_energy", plots_path)
+    plot_and_save(df, "elastic_kinetic_energy", plots_path)
+    plot_and_save(df, "gravitational_energy", plots_path)
+    plot_and_save(df, "momentumX", plots_path)
+    plot_and_save(df, "momentumY", plots_path)
+    plot_and_save(df, "momentumZ", plots_path)
+    plot_and_save(df, "plastic_moment", plots_path)
+    plot_and_save(df, "potency", plots_path)
+    plot_and_save(df, "seismic_moment", plots_path)
+    plot_and_save(df, "static_frictional_work", plots_path)
+    plot_and_save(df, "total_frictional_work", plots_path)
 
     print(f"Max M_0 : {np.max(df['M_0']):.10e}")
     print(f"Max M_w : {np.max(df['M_w'])}")
+
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        'mode',
-        choices=['before', 'after'],
-        help="Mode must be 'before' or 'after'"
+        "mode",
+        choices=["before", "after"],
+        help="Mode must be 'before' or 'after'",
     )
-    parser.add_argument('-p', '--parameter',default="parameters.par")
+    parser.add_argument("-p", "--parameter", default="parameters.par")
     args = parser.parse_args()
 
     par_file = args.parameter
 
     if not os.path.exists(par_file):
-        print("Cannot find parameters.par file",file=sys.stderr)
+        print("Cannot find parameters.par file", file=sys.stderr)
         exit(1)
     params = parse_parameters(par_file)
 
@@ -93,5 +133,6 @@ def main():
     elif args.mode == "after":
         after(params)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- SeisSol/plot_energy_files.py
+++ SeisSol/plot_energy_files.py
@@ -1,11 +1,35 @@
+import glob
 import os
-import pandas as pd
-import numpy as np
-import matplotlib.pylab as plt
 from pathlib import Path
+
+import matplotlib.pylab as plt
+import numpy as np
+import pandas as pd
 import typer
 from typing_extensions import Annotated, Optional
-import glob
+
+
+def get_units(variable_name):
+    units = {
+        "gravitational_energy": "J",
+        "acoustic_energy": "J",
+        "acoustic_kinetic_energy": "J",
+        "elastic_energy": "J",
+        "elastic_kinetic_energy": "J",
+        "momentumX": "kg·m/s",
+        "momentumY": "kg·m/s",
+        "momentumZ": "kg·m/s",
+        "total_frictional_work": "J",
+        "static_frictional_work": "J",
+        "seismic_moment": "N·m",
+        "potency": "m³",
+        "plastic_moment": "J",
+        "seismic_moment_rate": "N·m/s",
+        "M_0": "N·m",
+        "M_w": "",
+    }
+    return units.get(variable_name, "")
+
 
 def convert_to_df(filepath: Path) -> pd.DataFrame:
     if not os.path.exists(filepath):
@@ -13,16 +37,26 @@ def convert_to_df(filepath: Path) -> pd.DataFrame:
     df = pd.read_csv(filepath)
     df = df.pivot_table(index="time", columns="variable", values="measurement")
     df["seismic_moment_rate"] = np.gradient(df["seismic_moment"], df.index[1])
-    df["M_0"] = df["seismic_moment"] - df["total_frictional_work"] - df["elastic_energy"] - df["elastic_kinetic_energy"]
+    df["M_0"] = (
+        df["seismic_moment"]
+        - df["total_frictional_work"]
+        - df["elastic_energy"]
+        - df["elastic_kinetic_energy"]
+    )
     df["M_0"] = df["M_0"].where(df["M_0"] > 0)  # mask non-positive values
     df["M_w"] = (np.log10(df["M_0"]) - 9.1) / 1.5
     return df
 
-def plot_and_save_column(column_name: str, file_data_frames: list[tuple[str, pd.DataFrame]],output_path: Path):
+
+def plot_and_save_column(
+    column_name: str,
+    file_data_frames: list[tuple[str, pd.DataFrame]],
+    output_path: Path,
+):
     print(f"Plotting {column_name}")
     plt.xlabel("Time (s)")
-    plt.ylabel(column_name)
-    for filename,df in file_data_frames:
+    plt.ylabel(f"{column_name} in {get_units(column_name)}")
+    for filename, df in file_data_frames:
         if column_name in df.columns:
             time = df.index.to_numpy()
             y_axis = df[column_name].to_numpy()
@@ -33,8 +67,15 @@ def plot_and_save_column(column_name: str, file_data_frames: list[tuple[str, pd.
 
 
 def main(
-    energy_directory: Annotated[Path, typer.Argument(help="Directory containing all the energy files")],
-    output: Annotated[Optional[Path], typer.Option(help="Output path for the plots, will make direcory if it does not exist, by default will add plots folder to energy direcory")] = None
+    energy_directory: Annotated[
+        Path, typer.Argument(help="Directory containing all the energy files")
+    ],
+    output: Annotated[
+        Optional[Path],
+        typer.Option(
+            help="Output path for the plots, will make direcory if it does not exist, by default will add plots folder to energy direcory"
+        ),
+    ] = None,
 ):
     if not os.path.isdir(energy_directory):
         print(f"Could not find {energy_directory}!")
@@ -46,11 +87,14 @@ def main(
     output.mkdir(parents=True, exist_ok=True)
 
     csv_files = glob.glob(os.path.join(energy_directory, "*.csv"))
-    file_data_frames = [(Path(file).stem, convert_to_df(Path(file))) for file in csv_files]
-    all_columns = set.union(*[set(df.columns) for _, df in file_data_frames ])
+    file_data_frames = [
+        (Path(file).stem, convert_to_df(Path(file))) for file in csv_files
+    ]
+    all_columns = set.union(*[set(df.columns) for _, df in file_data_frames])
     for column in all_columns:
         output_path = output / f"{column}.svg"
-        plot_and_save_column(column, file_data_frames,output_path)
+        plot_and_save_column(column, file_data_frames, output_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     typer.run(main)
